519a84b2589212329534fa5a04323c2716c67bc6
Kalidash Odekare <shaktiadhikari49@gmail.com>
2026-06-29 13:14:35 +0600

    Removed dashboard components

--- src/features/dashboard/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { DashboardHeader } from "./dashboard-header";
-export { DashboardStats } from "./dashboard-stats";
-export { DashboardCharts } from "./dashboard-charts";
-export { DashboardStatus } from "./dashboard-status";
