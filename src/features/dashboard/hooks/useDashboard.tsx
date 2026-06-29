56fb752899be3dabdb338e6def9fb63283e1df1b
Kalidash Odekare <shaktiadhikari49@gmail.com>
2026-06-29 16:13:25 +0600

    Dashboard services folder add and decisions json added

--- src/features/dashboard/hooks/useDashboard.tsx
+++ src/features/dashboard/hooks/useDashboard.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useQuery } from "@tanstack/react-query";
-import { getDashboard } from "@/services/dashboard.service";
+import { getDashboard } from "../services/dashboard.service";
 
 export const useDashboard = () => {
   return useQuery({
