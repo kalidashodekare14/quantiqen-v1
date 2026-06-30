92bb97385bfc94606a4e379b0be89db80adbaade
Kalidash Odekare <shaktiadhikari49@gmail.com>
2026-06-30 08:01:21 +0600

    app page redirection:  if token have redirect /dashboard or nothing to login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,22 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [token, setToken] = useState<string | null>("");
+  const router = useRouter();
+
+  useEffect(() => {
+    const savedToken = localStorage.getItem("token");
+    setToken(savedToken);
+  }, []);
+
+  if (token) {
+    return router.push("/dashboard");
+  } else {
+    return router.push("/login");
+  }
+
   return (
     <div>
       <h1>Quantiqen v1</h1>
